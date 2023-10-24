e4a689318ac455eae05ba5dc3ba7fcdcccb878bb
Scott Blanch <scottglenblanch@gmail.com>
2023-10-24 16:50:43 -0600

    committing on Tue Oct 24 16:50:42 MDT 2023

--- sorting-algorithms/merge-sort/js/domMutator.js
+++ sorting-algorithms/merge-sort/js/domMutator.js
@@ -1,1 +1,11 @@
-// 
+import { numbersList } from "./state";
+// render list of numbers
+
+export function renderListOfNumbers() {
+    const element = document.querySelector('#initial-list-of-numbers');
+    const html = numbersList.reduce((accum, number) => {
+        return `${accum}<div>${number}</div>`
+    }, '');
+
+    element.innerHTML = html;
+}
--- sorting-algorithms/merge-sort/js/main.js
+++ sorting-algorithms/merge-sort/js/main.js
@@ -1,5 +1,9 @@
+import { renderListOfNumbers } from "./domMutator";
 // create the tree
 
 // render the tree
 
 // add event listener to when you click on the node you see the 2 children do the algorithm
+
+// render the numbers
+renderListOfNumbers();
